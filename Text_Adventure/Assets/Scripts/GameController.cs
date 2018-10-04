09ff3dfa98adf952f5e628f041415c133fe142af
sscoolqaz <seth.spears99@gmail.com>
2018-10-04 13:57:09 -0500

    added basic room

---
+++ Text_Adventure/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+
+       [HideInInspector] RoomNavigation roomNavigation
+	// Use this for initialization
+	void Awake () {
+        roomNavigation = GetComponent<RoomNavigation>();
+	}
+	
+    public void DisplayRoomText()
+    {
+        string combineText = roomNavigation.currentRoom.description + "/n";
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
