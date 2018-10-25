0721d5c2c83743a97594c14f463aace82f491bd8
sscoolqaz <seth.spears99@gmail.com>
2018-10-25 12:24:24 -0500

    got the intro to start working

--- Text_Adventure/Assets/Scripts/GameController.cs
+++ Text_Adventure/Assets/Scripts/GameController.cs
@@ -1,22 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
+    public Text displayText;
 
-       [HideInInspector] RoomNavigation roomNavigation
-	// Use this for initialization
-	void Awake () {
-        roomNavigation = GetComponent<RoomNavigation>();
-	}
-	
-    public void DisplayRoomText()
+    [HideInInspector] public RoomNavigation roomNavigation;
+
+    List<string> actionLog = new List<string>();
+
+    // Use this for initialization
+    void Awake ()
     {
-        string combineText = roomNavigation.currentRoom.description + "/n";
+        roomNavigation = GetComponent<RoomNavigation> ();
+	}
+
+    void Start()
+    {
+        DisplayRoomText ();
+        DisplayLoggedText ();
     }
 
+    public void DisplayLoggedText()
+    {
+        string logAsText = string.Join("\n", actionLog.ToArray ());
 
+        displayText.text = logAsText;
+    }
+
+    public void DisplayRoomText()
+    {
+        string combinedText = roomNavigation.currentRoom.description + "\n";
+
+        LogStringWithReturn (combinedText);
+    }
+
+    public void LogStringWithReturn(string stringToAdd)
+    {
+        actionLog.Add(stringToAdd + "\n");
+    }
+    
 	// Update is called once per frame
 	void Update () {
 		
